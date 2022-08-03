bd403ff2f6ce27b60f24ce32f85a951f8b8b2893
Htoo Myat <htoomyat.20399@gmail.com>
2022-08-03 15:31:35 +0630

    create project, add splash screen and app icon

---
+++ App.js
@@ -0,0 +1,17 @@
+import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
+
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
